0ed31dbe8c9fbf9e0c0c254ba72f6e2da9b302b8
Matt L <matt@Matts-MacBook-Pro-2.local>
2018-08-26 18:16:00 -0700

    get rid of code clutter

--- app/controllers/routes_controller.rb
+++ app/controllers/routes_controller.rb
@@ -148,60 +148,6 @@ class RoutesController < ApplicationController
     render status: 200, body: { answer: answer }.to_json
   end
 
-  # GET /routes/1
-  # GET /routes/1.json
-  def show
-  end
-
-  # GET /routes/new
-  def new
-    @route = Route.new
-  end
-
-  # GET /routes/1/edit
-  def edit
-  end
-
-  # POST /routes
-  # POST /routes.json
-  def create
-    @route = Route.new(route_params)
-
-    respond_to do |format|
-      if @route.save
-        format.html { redirect_to @route, notice: 'Route was successfully created.' }
-        format.json { render :show, status: :created, location: @route }
-      else
-        format.html { render :new }
-        format.json { render json: @route.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PATCH/PUT /routes/1
-  # PATCH/PUT /routes/1.json
-  def update
-    respond_to do |format|
-      if @route.update(route_params)
-        format.html { redirect_to @route, notice: 'Route was successfully updated.' }
-        format.json { render :show, status: :ok, location: @route }
-      else
-        format.html { render :edit }
-        format.json { render json: @route.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /routes/1
-  # DELETE /routes/1.json
-  def destroy
-    @route.destroy
-    respond_to do |format|
-      format.html { redirect_to routes_url, notice: 'Route was successfully destroyed.' }
-      format.json { head :no_content }
-    end
-  end
-
   private
 
     def find_origin_and_destination
